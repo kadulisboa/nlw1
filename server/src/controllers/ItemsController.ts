70268d832fd270af60e52227948025b669784baf
Kadu Lisboa <kadulisboa301@icloud.com>
2021-03-14 17:27:45 -0300

    changes

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,5 +1,21 @@
-class ItemsController {
+import knex from '../database/connection';
+import { Request, Response } from 'express';
 
+class ItemsController {
+    async index(resquest: Request, response: Response) {
+        const items = await knex('items').select('*');
+
+        const serializedItems = items.map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+                image: item.image,
+                image_url: `http://localhost:3333/uploads/${item.image}`
+            }
+        })
+
+        return response.status(200).json(serializedItems);
+    }
 }
 
 export default ItemsController;
